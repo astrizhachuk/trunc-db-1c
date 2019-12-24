ced2676c710542d8391716c2c40fd97d647669b7
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2019-12-24 22:00:18 +0300

    Первичная настройка сборки

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,55 @@
 plugins {
     java
+    id("io.franzbecker.gradle-lombok") version "3.2.0"
 }
 
 group = "ru.astrizhachuk"
+// TODO: 24.12.2019  auto.sem.ver
 version = "0.1"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    // https://mvnrepository.com/artifact/commons-cli/commons-cli
-    compile("commons-cli:commons-cli:1.4")
+val junitVersion = "5.5.2"
 
-    testCompile("junit", "junit", "4.12")
+dependencies {
+
+    // https://mvnrepository.com/artifact/commons-cli/commons-cli
+    implementation("commons-cli", "commons-cli", "1.4")
+
+    implementation("org.slf4j", "slf4j-api", "1.8.0-beta4")
+    implementation("org.slf4j", "slf4j-simple", "1.8.0-beta4")
+
+    compileOnly("org.projectlombok", "lombok", lombok.version)
+
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", junitVersion)
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
+    testImplementation("org.assertj", "assertj-core", "3.13.2")
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "ru.astrizhachuk.Main"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+
+    reports {
+        html.isEnabled = true
+    }
+}
+
+lombok {
+    version = "1.18.10"
 }
