08770852449e452d0d6e814b72b8d97040bf3b95
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2020-01-04 11:37:55 +0300

    Missed file

---
+++ src/main/java/ru/astrizhachuk/reporter/ReporterFactory.java
@@ -0,0 +1,16 @@
+package ru.astrizhachuk.reporter;
+
+import org.apache.commons.cli.CommandLine;
+
+public class ReporterFactory {
+
+    public static Reporter create(CommandLine cmd) {
+        String destOutputFile = cmd.getOptionValue("o", "");
+        if (destOutputFile.isEmpty()) {
+            return new ConsoleReporter();
+        } else {
+            return new FileReporter(destOutputFile);
+        }
+    }
+
+}
